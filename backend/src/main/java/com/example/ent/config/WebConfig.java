34fff34b349100c0dbcb98298a71dcdd57d56106
LaCalme <mironnaydanov@gmail.com>
2026-09-24 10:53:24 +0500

    Web config fix

--- backend/src/main/java/com/example/ent/config/WebConfig.java
+++ backend/src/main/java/com/example/ent/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
