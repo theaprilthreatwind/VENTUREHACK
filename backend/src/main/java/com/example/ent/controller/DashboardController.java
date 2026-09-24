c7101e7495ac85de984f107b08d18a70af6ff5d9
LaCalme <mironnaydanov@gmail.com>
2026-09-24 15:31:15 +0500

    AI prompt fix

--- backend/src/main/java/com/example/ent/controller/DashboardController.java
+++ backend/src/main/java/com/example/ent/controller/DashboardController.java
@@ -6,6 +6,7 @@ import com.example.ent.service.DashboardService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class DashboardController {
     private final DashboardService dashboardService;
+    private final ChatClient chatClient;
 
     @PutMapping("/{userId}/topics/{topicId}/target-score")
     public void setTargetScore(@PathVariable Long userId,
@@ -34,10 +36,20 @@ public class DashboardController {
     @GetMapping("/study-plan")
     public String generatePlan(
             @RequestParam Long userId,
-            @RequestParam(required = false) Long daysUntilExam) {
-
+            @RequestParam(required = false) Long daysUntilExam,
+            HttpServletRequest request) {
+        log.info("Выполнен запрос по эндпоинту: '{} {}', Строка параметров запроса: '{}'", request.getMethod(), request.getRequestURI(), request.getQueryString());
         String aiResponse = dashboardService.getStudyPlanForUser(userId, daysUntilExam);
 
         return aiResponse;
+    }
+
+    @GetMapping("/test-ai")
+    public String testAi(HttpServletRequest request) {
+        log.info("Выполнен запрос по эндпоинту: '{} {}', Строка параметров запроса: '{}'", request.getMethod(), request.getRequestURI(), request.getQueryString());
+        return chatClient.prompt()
+                .user("Привет! Скажи слово 'Работает', если ты меня слышишь.")
+                .call()
+                .content();
     }
 }
